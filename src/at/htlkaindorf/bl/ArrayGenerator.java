2ac88ee0c4f35f1582d67be128b811b4898cf73d
muethb20 <muethb20@htl-kaindorf.at>
2023-01-24 12:59:17 +0100

    Finished Class ArrayGenerator

--- src/at/htlkaindorf/bl/ArrayGenerator.java
+++ src/at/htlkaindorf/bl/ArrayGenerator.java
@@ -1,5 +1,17 @@
 package at.htlkaindorf.bl;
 
+import java.util.Random;
+
 public class ArrayGenerator {
+    public static int[] generateArray(int size){
+        int[] array = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+        return array;
+    }
 
 }
